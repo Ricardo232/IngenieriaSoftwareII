920b2cb5c283b505b265c4a1f75983cfcae471ca
DiegoMego <30615834+DiegoMego@users.noreply.github.com>
2017-10-02 16:59:05 -0500

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -2,7 +2,7 @@ import pygame as pg
 import random
 from os import *
 from settings import *
-from keyhandler import *
+import math
 vec = pg.math.Vector2
 
 class SpriteSheet:
@@ -22,12 +22,11 @@ class Player(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         self.game = game
         self.clase = "Warrior"
-        self.keys = KeyHandler()
         self.load_images()
         self.current_frame = 0
         self.last_dir = "down"
         self.last_update = 0
-        self.image = self.player_movement["down"][3]
+        self.image = self.player_movement["down"][0]
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.vel = vec(0, 0)
@@ -125,6 +124,7 @@ class Player(pg.sprite.Sprite):
         if keys[pg.K_q]:
             self.ismoving = False
             self.vel = (0, 0)
+            
             self.action(self.player_attack, self.last_dir)
         else:
             self.ismoving = True
@@ -139,6 +139,151 @@ class Player(pg.sprite.Sprite):
         self.pos += self.vel
         self.rect.centerx = self.pos.x
         self.rect.centery = self.pos.y
+
+    def action(self, action_type, action_dir):
+        self.last_dir = action_dir
+        now = pg.time.get_ticks()
+        if now - self.last_update > 100:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(action_type[action_dir])
+            self.image = action_type[action_dir][self.current_frame]
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.clase = "Felltwin"
+        self.load_images()
+        self.current_frame = 0
+        self.last_dir = "down"
+        self.last_update = 0
+        self.image = self.mob_movement["down"][self.current_frame]
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.vel = vec(0, 0)
+        self.pos = vec(WIDTH / 2, 0)
+        self.rect.center = self.pos
+
+    def load_images(self):
+        self.spritesheet = SpriteSheet("Enemies\\" + self.clase, self.clase + ".png")
+        self.mob_movement = {"down": [],
+                             "downleft": [],
+                             "left": [],
+                             "upleft": [],
+                             "up": [],
+                             "upright": [],
+                             "right": [],
+                             "downright": []}
+        y = 1045
+        for key in self.mob_movement:
+            for x in range(3074, 4738, 128):
+                self.mob_movement[key].append(self.spritesheet.get_image(x, y, 128, 128, 0, 0))
+            y += 129
+
+        self.mob_attack = {"down": [],
+                           "downleft": [],
+                           "left": [],
+                           "upleft": [],
+                           "up": [],
+                           "upright": [],
+                           "right": [],
+                           "downright": []}
+
+        y = 7
+        for key in self.mob_attack:
+            for x in range(0, 2048, 128):
+                self.mob_attack[key].append(self.spritesheet.get_image(x, y, 128, 128, 0, -12))
+            y += 129
+
+        self.mob_standing = {"down": [],
+                             "downleft": [],
+                             "left": [],
+                             "upleft": [],
+                             "up": [],
+                             "upright": [],
+                             "right": [],
+                             "downright": []}
+        y = 1045
+        for key in self.mob_standing:
+            for x in range(961, 2881, 96):
+                self.mob_standing[key].append(self.spritesheet.get_image(x, y, 96, 96))
+            y += 97
+
+    def update(self):
+        self.vel = vec(0, 0)
+        self.detect()
+        self.pos += self.vel
+        self.rect.centerx = self.pos.x
+        self.rect.centery = self.pos.y
+        # if self.rect.bottom > HEIGHT:
+        #     self.rect.right = WIDTH
+        # if self.rect.left < 0:
+        #     self.rect.left = 0
+
+    def detect(self):
+        if math.sqrt((self.rect.centerx - self.game.player.rect.centerx) ** 2 + (self.rect.centery - self.game.player.rect.centery) ** 2) < 500:
+            self.follow()
+
+    def follow(self):
+        self.choque = False
+        offset_top = 64
+        offset_side = 96
+        offset_y_right = 20
+        offset_y_left = 16
+        if self.rect.left <= self.game.player.rect.right - offset_side and self.rect.right >= self.game.player.rect.left + offset_side:
+            if self.rect.bottom == self.game.player.rect.top + offset_top:
+                self.choque = True
+                self.vel = [0, 0]
+                self.attack()
+            elif self.rect.top == self.game.player.rect.bottom - offset_top:
+                self.choque = True
+                self.vel = [0, 0]
+                self.attack()
+
+        if self.rect.top <= self.game.player.rect.bottom - offset_top and self.rect.bottom >= self.game.player.rect.top + offset_top:
+            if self.rect.left == self.game.player.rect.right - offset_side:
+                self.choque = True
+                self.vel = [0, 0]
+                self.attack()
+            elif self.rect.right == self.game.player.rect.left + offset_side:
+                self.choque = True
+                self.vel = [0, 0]
+                self.attack()
+
+        if self.rect.centerx > self.game.player.rect.centerx and not self.choque:
+            if self.rect.centery > self.game.player.rect.centery - offset_y_left:
+                self.vel.x = -MOB_SPEED
+                self.vel.y = -MOB_SPEED
+                self.action(self.mob_movement, "upleft")
+            elif self.rect.centery < self.game.player.rect.centery - offset_y_left:
+                self.vel.x = -MOB_SPEED
+                self.vel.y = MOB_SPEED
+                self.action(self.mob_movement, "downleft")
+            else:
+                self.vel.x = -MOB_SPEED
+                self.action(self.mob_movement, "left")
+        elif self.rect.centerx < self.game.player.rect.centerx and not self.choque:
+            if self.rect.centery > self.game.player.rect.centery - offset_y_right:
+                self.vel.x = MOB_SPEED
+                self.vel.y = -MOB_SPEED
+                self.action(self.mob_movement, "upright")
+            elif self.rect.centery < self.game.player.rect.centery - offset_y_right:
+                self.vel.x = MOB_SPEED
+                self.vel.y = MOB_SPEED
+                self.action(self.mob_movement, "downright")
+            else:
+                self.vel.x = MOB_SPEED
+                self.action(self.mob_movement, "right")
+        elif not self.choque:
+            if self.rect.centery > self.game.player.rect.centery:
+                self.vel.y = -MOB_SPEED
+                self.action(self.mob_movement, "up")
+            elif self.rect.centery < self.game.player.rect.centery:
+                self.vel.y = MOB_SPEED
+                self.action(self.mob_movement, "down")
+
+    def attack(self):
+        pass
 
     def action(self, action_type, action_dir):
         self.last_dir = action_dir
